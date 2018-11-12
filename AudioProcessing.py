78b41061201aae31d95ea3c3bb6c6b906dc8514d
fbleite <feliperbleite@gmail.com>
2018-11-11 20:15:45 -0500

    modifications based upon testing in the raspberry pi, it was throwing buffer overflow errors

--- AudioProcessing.py
+++ AudioProcessing.py
@@ -5,10 +5,11 @@ import numpy as np
 class AudioProcessing :
     def __init__(self,
                  chunk=1024,
+                 # chunk=2048,
                  rate=44100):
         self.chunk = chunk
         self.rate = rate
-        self.format = pyaudio.paInt16
+        self.format = pyaudio.paInt8
         self.channels = 1
         self.timebox = 5 #In seconds
         self.player = pyaudio.PyAudio()
@@ -17,10 +18,12 @@ class AudioProcessing :
         allSound = []
         stream = self.player.open(format=self.format, channels=1, rate=self.rate, input=True, frames_per_buffer=self.chunk)
         for i in range(int(self.timebox * self.rate / self.chunk)):
-            sound = stream.read(self.chunk)
-            allSound.extend(np.fromstring(sound, dtype=np.int16))
+            sound = stream.read(self.chunk, exception_on_overflow= False)
+            allSound.extend(np.fromstring(sound, dtype=np.int8))
         stream.stop_stream()
         stream.close()
+        print('All Sound')
+        print(allSound)
         return self.calculateAbsotuteAverage(allSound)
 
 
