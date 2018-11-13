49bea9b23c09a05ae0c7553ceba28d4d902da3b5
Felipe Raspberry <feliperbleite@gmail.com>
2018-11-13 12:27:41 +0000

    A little more heuristic after testing in the raspberry

--- AudioProcessing.py
+++ AudioProcessing.py
@@ -4,12 +4,11 @@ import numpy as np
 
 class AudioProcessing :
     def __init__(self,
-                 chunk=1024,
-                 # chunk=2048,
+                 chunk=8192,
                  rate=44100):
         self.chunk = chunk
         self.rate = rate
-        self.format = pyaudio.paInt8
+        self.format = pyaudio.paInt16
         self.channels = 1
         self.timebox = 5 #In seconds
         self.player = pyaudio.PyAudio()
@@ -19,11 +18,13 @@ class AudioProcessing :
         stream = self.player.open(format=self.format, channels=1, rate=self.rate, input=True, frames_per_buffer=self.chunk)
         for i in range(int(self.timebox * self.rate / self.chunk)):
             sound = stream.read(self.chunk, exception_on_overflow= False)
-            allSound.extend(np.fromstring(sound, dtype=np.int8))
+            #sound = stream.read(self.chunk)
+            allSound.extend(np.fromstring(sound, dtype=np.int16))
         stream.stop_stream()
         stream.close()
         print('All Sound')
-        print(allSound)
+        #allSound = [ elem for elem in  allSound if elem != 0]
+        # print(allSound)
         return self.calculateAbsotuteAverage(allSound)
 
 
--- LaundryMachineSensor.py
+++ LaundryMachineSensor.py
@@ -1,18 +1,22 @@
 from AudioProcessing import AudioProcessing
 from SimpleAlertLog import SimpleAlertLog as Alert
 import numpy as np
+import datetime
 
-ap = AudioProcessing(chunk=1024, rate=44100)
+# ap = AudioProcessing(chunk=2048, rate=44100)
+ap = AudioProcessing(chunk=8192, rate=44100)
 previousIntensities = [0, 0, 0]
 wasLaundryMachineRunningBefore = False
 threshold = 1500
 needsToAlert = False
 
 
+
 while True:
     del previousIntensities[0]
     previousIntensities.append(ap.getSoundIntensityForTimebox())
     averageIntensities = np.mean(previousIntensities)
+    print (datetime.datetime.now())
     print('average: ', averageIntensities)
     if averageIntensities < threshold:
         if wasLaundryMachineRunningBefore:
